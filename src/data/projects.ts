59d00ceddd2bce3249e86320def9d7f88147ccc9
Utkarsh Tripathi <shreytripathi2004@gmail.com>
2025-02-21 14:55:03 +0530

    deployment link added to that new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -31,7 +31,7 @@ export const projects = [
     "title": "Employee Management System",
     "description": "A employee management system with CRUD operations, built using ReactJS and Node.js",
     "techStack": ["JavaScript", "React", "Vite", "Node.js"],
-    "githubUrl": "https://github.com/Utkarsh-152/Employee-management-System.git",
+    "githubUrl": "https://employee-management-system-f9ue.vercel.app/",
     "image": "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   } 
 
